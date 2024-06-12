65f2b3f7d93a8be2cdd2be36de480a0d32ea62aa
SunnnySong <tjsghktjswls@naver.com>
2024-06-12 14:47:38 +0900

    feat: 홈 화면 첫번째 section- 영화 스크롤뷰

--- flutter_movie_app/lib/sources/screens/home/home.dart
+++ flutter_movie_app/lib/sources/screens/home/home.dart
@@ -104,54 +104,101 @@ class _FirstSectionState extends State<FirstSection> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 24, bottom: 20),
-      child: Row(
+      child: Column(
         children: [
-          Button(
-            onPressed: () {
-              _toggleSelected();
-            },
-            isSelected: _isSelected,
-            deselectedStyle: ButtonDecorationStyle(
-              backgroundColor: Colors.transparent,
-              foregroundColor: const Color(0xFFCCCCCC),
-              borderColor: const Color(0xFF444444),
-              borderWidth: 1,
-            ),
-            selectedStyle: ButtonDecorationStyle(
-              backgroundColor: const Color(0xFFe64980),
-              foregroundColor: Colors.white,
-            ),
-            text: "영화개봉순",
-            cornerRadius: 48,
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-            verticalPadding: 13,
-            horizantalPadding: 16,
+          Row(
+            children: [
+              Button(
+                onPressed: () {
+                  _toggleSelected();
+                },
+                isSelected: _isSelected,
+                deselectedStyle: ButtonDecorationStyle(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: const Color(0xFFCCCCCC),
+                  borderColor: const Color(0xFF444444),
+                  borderWidth: 1,
+                ),
+                selectedStyle: ButtonDecorationStyle(
+                  backgroundColor: const Color(0xFFe64980),
+                  foregroundColor: Colors.white,
+                ),
+                text: "영화개봉순",
+                cornerRadius: 48,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                verticalPadding: 13,
+                horizantalPadding: 16,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Button(
+                onPressed: () {
+                  _toggleSelected();
+                },
+                isSelected: !_isSelected,
+                deselectedStyle: ButtonDecorationStyle(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: const Color(0xFFCCCCCC),
+                  borderColor: const Color(0xFF444444),
+                  borderWidth: 1,
+                ),
+                selectedStyle: ButtonDecorationStyle(
+                  backgroundColor: const Color(0xFFe64980),
+                  foregroundColor: Colors.white,
+                ),
+                text: "예매율순",
+                cornerRadius: 48,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                verticalPadding: 13,
+                horizantalPadding: 16,
+              ),
+            ],
           ),
           const SizedBox(
-            width: 12,
+            height: 16,
           ),
-          Button(
-            onPressed: () {
-              _toggleSelected();
-            },
-            isSelected: !_isSelected,
-            deselectedStyle: ButtonDecorationStyle(
-              backgroundColor: Colors.transparent,
-              foregroundColor: const Color(0xFFCCCCCC),
-              borderColor: const Color(0xFF444444),
-              borderWidth: 1,
+          SizedBox(
+            height: 260,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 140,
+                  margin: const EdgeInsets.only(right: 16),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 140,
+                        height: 198,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://image.tmdb.org/t/p/w500/6Wdl9N6dL0Hi0T1qJLWSz6gMLbd.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const Text(
+                        '극장판 귀멸의 칼날: 무한열차편',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-            selectedStyle: ButtonDecorationStyle(
-              backgroundColor: const Color(0xFFe64980),
-              foregroundColor: Colors.white,
-            ),
-            text: "예매율순",
-            cornerRadius: 48,
-            fontSize: 14,
-            fontWeight: FontWeight.bold,
-            verticalPadding: 13,
-            horizantalPadding: 16,
           ),
         ],
       ),
